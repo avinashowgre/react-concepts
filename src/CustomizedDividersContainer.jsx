593bd7264a4fb723ffd4ac7d90ac39571846fa67
avinashowgre <41430980+avinashowgre@users.noreply.github.com>
2022-12-23 17:03:31 -0600

    1 file modified

--- src/CustomizedDividersContainer.jsx
+++ src/CustomizedDividersContainer.jsx
@@ -2,16 +2,52 @@ import React, { useState } from 'react';
 
 import { CustomizedDividers } from './CustomizedDividers';
 
+function hexToRgbA(hex) {
+  var c;
+  if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+    c = hex.substring(1).split('');
+    if (c.length == 3) {
+      c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+    }
+    c = '0x' + c.join('');
+    return (
+      'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',0.2)'
+    );
+  }
+  throw new Error('Bad Hex');
+}
+
 export function CustomizedDividersContainer() {
   const [styles, setStyles] = useState({
-    color: '',
+    color: '#000000',
     font: '',
+    fontSize: '',
   });
 
   const handleStyleChange = (styles) => {
-    console.log(styles);
+    setStyles(styles);
   };
   return (
-    <CustomizedDividers styles={styles} onStyleChange={handleStyleChange} />
+    <div
+      style={{
+        alignItems: 'flex-start',
+        display: 'flex',
+        columnGap: 10,
+      }}
+    >
+      <pre
+        style={{
+          border: `1px solid ${styles.color}`,
+          boxShadow: `0 8px 16px 0 ${hexToRgbA(styles.color)}`,
+          fontSize: `${styles.fontSize}`,
+          padding: 10,
+          height: 120,
+          width: 400,
+        }}
+      >
+        <code>{JSON.stringify(styles, null, 4)}</code>
+      </pre>
+      <CustomizedDividers styles={styles} onStyleChange={handleStyleChange} />
+    </div>
   );
 }
